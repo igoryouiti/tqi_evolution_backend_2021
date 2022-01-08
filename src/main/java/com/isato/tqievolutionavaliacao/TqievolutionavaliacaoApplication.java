9cc4d1cdd4e51e4b239adc1ccb3f752744b19a45
Igor Sato <igoryouiti@hotmail.com>
2022-01-08 12:57:25 -0300

    first commit

---
+++ src/main/java/com/isato/tqievolutionavaliacao/TqievolutionavaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.isato.tqievolutionavaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqievolutionavaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqievolutionavaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isato/tqievolutionavaliacao/TqievolutionavaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isato.tqievolutionavaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqievolutionavaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
